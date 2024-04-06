3018f0185115d4cba9b42b433daa5ecfbb137430
kingbonwa <kingbonwa@hotmail.co.za>
2024-04-06 04:55:47 +0200

    error fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Property Na Tendani",
+  description: "The fastest and easiest way to trade in your property",
 };
 
 export default function RootLayout({
